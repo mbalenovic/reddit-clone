a0a99c7407fc3825b6b0a37bcb24c90c37b14873
mbalenovic <mile.balenovic@gmail.com>
2025-10-07 23:30:50 +0200

    update tsconfig & add generated filed

---
+++ server/migrations/Migration20251001162045.ts
@@ -0,0 +1,14 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20251001162045 extends Migration {
+
+  override async up(): Promise<void> {
+    this.addSql(`create table "user" ("id" serial primary key, "created_at" timestamptz not null, "updated_at" timestamptz not null, "username" text not null, "email" varchar(255) not null, "password" varchar(255) not null);`);
+    this.addSql(`alter table "user" add constraint "user_username_unique" unique ("username");`);
+  }
+
+  override async down(): Promise<void> {
+    this.addSql(`drop table if exists "user" cascade;`);
+  }
+
+}
