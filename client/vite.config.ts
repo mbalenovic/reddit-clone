13e8440d609a9abd7c11adaf8413565fe89526e7
mbalenovic <mile.balenovic@gmail.com>
2025-12-29 15:06:24 +0100

    remove nitroV2Plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tanstackRouter from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
-import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    nitroV2Plugin(),
-    TanStackRouterVite({ autoCodeSplitting: true }),
+    tanstackRouter({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
   ],
